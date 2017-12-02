c2682676297f851aa21119c50c85796cb8f1b668
Brett Gildersleeve <brett.gildersleeve@gmail.com>
2017-12-02 10:49:20 -0800

    Simplified player locomotion (for now)

--- Assets/Scripts/LocomotionController.cs
+++ Assets/Scripts/LocomotionController.cs
@@ -3,26 +3,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class LocomotionController : MonoBehaviour {
 
 	public float speed = 2f;
+	//private CapsuleCollider2D colliderComponent;
 
 	// Use this for initialization
 	void Start () {
-		
+		//colliderComponent = GetComponent<CapsuleCollider2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float inputX = Input.GetAxis("Horizontal");
 		float inputY = Input.GetAxis("Vertical");
-		UpdatePosition(inputX, inputY);
+		Vector2 targetPosition = CalculateTargetPosition(transform.position, inputX, inputY);
+		transform.position = targetPosition;
 	}
 
-	private void UpdatePosition(float inputX, float inputY) {
-        float translationX = (inputX * speed * Time.deltaTime);
-		float translationY = (inputY * speed * Time.deltaTime);
-		transform.Translate(translationX, translationY, 0);
-    }
+	private Vector2 CalculateTargetPosition(Vector3 currentPosition, float inputX, float inputY) {
+		float currentX = currentPosition.x;
+		float currentY = currentPosition.y;
+		float targetX = currentX + (inputX * speed * Time.deltaTime);
+		float targetY = currentY + (inputY * speed * Time.deltaTime);
+		return new Vector2(targetX, targetY);
+	}
+
+	// private bool CanMove(Vector2 moveTarget) {
+	// 	Vector2 size = colliderComponent.size;
+	// 	CapsuleDirection2D direction = colliderComponent.direction;
+	// 	Collider2D[] colliders = Physics2D.OverlapCapsuleAll(moveTarget, size, direction, 0f);
+	// 	foreach (Collider2D c in colliders) {
+	// 		Debug.Log(c.ToString());
+	// 	}
+	// 	return true;
+	// }
 }
 
