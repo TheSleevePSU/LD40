b26f0851113e4952755b89a9f02173a27adb0a2c
ellkell <ellkell.git@gmail.com>
2017-12-02 14:31:13 -0800

    changing movement to rigidBody2d

--- Assets/Scripts/LocomotionController.cs
+++ Assets/Scripts/LocomotionController.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
 public class LocomotionController : MonoBehaviour {
-
+	//public Rigidbody rb = GetComponent<Rigidbody>();
 	public float maxSpeed = 2f;
+
+	Animator anim;
 
 	// Use this for initialization
 	void Start () {
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -20,6 +23,15 @@ public class LocomotionController : MonoBehaviour {
 		transform.position = targetPosition;
 	}
 
+	//A tutorial I was using ran all physics throuch a fixed update instead of reg update
+	/*void FixedUpdate(){
+	 * float inputX = Input.GetAxis("Horizontal");
+	 * float inputY = Input.GetAxis("Vertical");
+	 * rb.velocity = new Vector2(inputX * maxSpeed, inputY * maxSpeed);
+	 * 
+	 * anim.SetFloat("speedMoving", Mathf.Abs()
+	 * }
+	*/
 	private Vector2 CalculateTargetPosition(Vector3 currentPosition, float inputX, float inputY) {
 		float currentX = currentPosition.x;
 		float currentY = currentPosition.y;
