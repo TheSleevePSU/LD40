afd0d26cfbaf07f321a39fd51926a684dabf5f79
Brett Gildersleeve <brett.gildersleeve@gmail.com>
2017-12-01 21:44:15 -0800

    - Added OrientSpriteToMouse - Created empty gameObject to test OrientSpriteToMouse

---
+++ Assets/Scripts/OrientSpriteToMouse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class OrientSpriteToMouse : MonoBehaviour {
+
+	private Camera gameCamera;
+	private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		gameCamera = FindObjectOfType<Camera>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 worldPoint = gameCamera.ScreenToWorldPoint(Input.mousePosition);
+		if (worldPoint.x > transform.position.x) {
+			spriteRenderer.flipX = true;
+		} else {
+			spriteRenderer.flipX = false;
+		}
+	}
+}
+
